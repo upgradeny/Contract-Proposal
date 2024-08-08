55adb7f2a1656d576aaa0eccd5f08645e2518cad
upgradeny <98571704+upgradeny@users.noreply.github.com>
2024-08-08 20:48:09 +0500

    Update calc_main.js

--- js/calc_main.js
+++ js/calc_main.js
@@ -257,7 +257,7 @@ $(document).ready(function(){
 				
 				
 				var fifty_remaining_1 =  ( round_2_digits( totalDue - retainer_paid - fifty_due_1 ) );
-				fifty_remaining_1 = fifty_remaining_1 + (retainer_paid/2)
+				
 				//var fifty_remaining_1_date =  jQuery('#fifty_remaining_1_date').val() ;
 				
 				jQuery('#pdf_fifty_remaining_1').text( `$ ${numberWithCommas ( round_2_digits( fifty_remaining_1 ) )}`);
